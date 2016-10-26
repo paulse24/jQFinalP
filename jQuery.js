985e383a1538cdc5cf1784e99f650a17f7a6ed60
paulse24 <pauls@tgs.org>
2016-10-26 12:21:59 +0100

    built basic grid

--- jQuery.js
+++ jQuery.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
+ for (i=0; i < 16*16; i++) {
+$('.container').append("<div class=standard></div>");
+};
 
-})
+$('div.standard').hover(
+  function(){
+  $(this).addClass('hovered');}
+);
+
+});
