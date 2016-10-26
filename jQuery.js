1d949139a8fb143e62566e8a2eeae5fe1080c8cc
paulse24 <pauls@tgs.org>
2016-10-26 16:37:43 +0100

    final version without option

--- jQuery.js
+++ jQuery.js
@@ -1,11 +1,33 @@
+var gn = 16
+
 $(document).ready(function() {
- for (i=0; i < 16*16; i++) {
+
+ for (i=0; i < gn*gn; i++) {
 $('.container').append("<div class=standard></div>");
 };
 
 $('div.standard').hover(
   function(){
-  $(this).addClass('hovered');}
+$(this).addClass('hovered');}
 );
+
+document.getElementById('reset').onclick = function newgrid() {
+  gn = prompt("What size grid would you like to generate?");
+  $('.standard').remove();
+
+  for (i=0; i < gn*gn; i++) {
+ $('.container').append("<div class=standard></div>");
+ };
+
+ $('div.standard').hover(
+   function(){
+ $(this).addClass('hovered');}
+ );
+
+ var ghw = 640/gn;
+  $('.standard').css("height", ghw+"px");
+  $('.standard').css("width", ghw+"px");
+
+};
 
 });
